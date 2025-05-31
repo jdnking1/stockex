2bbcddb0b4a90f4906ef3fcb7d53262622377ee7
spiraln <jdnking4@gmail.com>
2025-06-01 00:12:51 +0200

    fix compile error in memory_pool

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <print>
 
+#include <utils/memory_pool.hpp>
+
 int main() {
-    std::print("w {} \n", 23);
+  stockex::utils::MemoryPool<double> x{22};
+
+  auto ptr = x.alloc(22);
+
+  x.free(ptr);
+
+  std::print("w {}  {}\n", 23, *ptr);
 }
--- src/utils/memory_pool.hpp
+++ src/utils/memory_pool.hpp
@@ -14,7 +14,7 @@ public:
     memory_.resize(size);
 
     for (std::size_t i = 0; i < size - 1; i++) {
-      memory_[i].next_free_block_ = i + 1;
+      memory_[i].next_ = i + 1;
     }
 
     ASSERT(static_cast<void *>(&(memory_[0].data_)) ==
@@ -25,11 +25,11 @@ public:
   template <typename... Args> T *alloc(Args &&...args) {
     ASSERT(freeBlockCount_ > 0, "No free memory blocks.");
     auto memory_block = &memory_[freeBlockIndex_];
-    ASSERT(memory_block->is_free_, "Memory block is not free.");
-    memory_block->is_free_ = false;
+    ASSERT(memory_block->isFree_, "Memory block is not free.");
+    memory_block->isFree_ = false;
     auto result = reinterpret_cast<T *>(&memory_[freeBlockIndex_].data_);
     result = new (result) T(std::forward<Args>(args)...);
-    freeBlockIndex_ = memory_[freeBlockIndex_].next_free_block_;
+    freeBlockIndex_ = memory_[freeBlockIndex_].next_;
     --freeBlockCount_;
     return result;
   }
@@ -37,10 +37,10 @@ public:
   T *rawAlloc() {
     ASSERT(freeBlockCount_ > 0, "No free memory blocks.");
     auto *memory_block = &memory_[freeBlockIndex_];
-    ASSERT(memory_block->is_free_, "Memory block is not free.");
-    memory_block->is_free_ = false;
+    ASSERT(memory_block->isFree_, "Memory block is not free.");
+    memory_block->isFree_ = false;
     std::size_t current_index = freeBlockIndex_;
-    freeBlockIndex_ = memory_block->next_free_block_;
+    freeBlockIndex_ = memory_block->next_;
     freeBlockCount_--;
     return reinterpret_cast<T *>(&memory_[current_index].data_);
   }
@@ -50,9 +50,9 @@ public:
         reinterpret_cast<const MemoryBlock *>(ptr) - &memory_[0];
     ASSERT(block_index < memory_.size(), "Invalid memory block index.");
     auto memory_block = &memory_[block_index];
-    ASSERT(!memory_block->is_free_, "Memory block is already free.");
-    memory_block->is_free_ = true;
-    memory_block->next_free_block_ = freeBlockIndex_;
+    ASSERT(!memory_block->isFree_, "Memory block is already free.");
+    memory_block->isFree_ = true;
+    memory_block->next_ = freeBlockIndex_;
     freeBlockIndex_ = block_index;
     freeBlockCount_++;
   }
@@ -67,7 +67,7 @@ private:
   struct MemoryBlock {
     alignas(T) char data_[sizeof(T)];
     std::size_t next_ = std::numeric_limits<std::size_t>::max();
-    bool isFree = true;
+    bool isFree_ = true;
   };
 
   std::vector<MemoryBlock> memory_;
